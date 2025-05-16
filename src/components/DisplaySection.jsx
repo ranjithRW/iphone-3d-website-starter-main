e5769b94f28d6aec76077bd7ba8d59a4942b1943
Ranjith.T <121870537+ranjiththangavel2000@users.noreply.github.com>
2025-05-16 15:25:13 +0530

    feat(DisplaySection): change title class from 'title' to 'text'

--- src/components/DisplaySection.jsx
+++ src/components/DisplaySection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function DisplaySection() {
     return (
         <div className="display-section wrapper">
-            <h2 className="title">New</h2>
+            <h2 className="text">New</h2>
             <p className="text">Brilliant</p>
             <span className="description">A display thats up to2x brighter in the sun.</span>
             <button className="button">Try me!</button>
